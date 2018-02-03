69af4381a5d930440187740bf00eec5b62e099f4
Maxime Martens <stalker2106x@gmail.com>
2018-02-03 04:58:28 +0100

    indented tests.

--- tests/test.cpp
+++ tests/test.cpp
@@ -12,33 +12,43 @@ int main(int argc, char **argv)
 
     std::cout << "TinyConf Tests:\n";
     std::cout << "Setting tests into config\n";
+
     test.set("CharTest", "OK");
     test.set("StringTest", str);
     test.set("IntTest", integer);
     test.set("DoubleTest", decimal);
     test.setArray("ArrayTest", array);
     test.setAggregate("PairTest", pair);
+
     std::cout << "Serializing to file \"" << test.getPath() << "\"\n";
     test.save();
+
     test.relocate("./test.cfg");  
     std::cout << "Reloading file \"" << test.getPath() << "\"\n";
+
     std::cout << "Char Test => ";
     test.get("CharTest", sChar);
     std::cout << (strcmp(sChar, "OK") == 0 ? "OK" : "FAIL") << "\n";
+
     std::cout << "String Test => ";
     test.get("StringTest", sStr);
     std::cout << (sStr == "OK" ? "OK" : "FAIL") << "\n";
+
     std::cout << "Int Test => ";
     test.get("IntTest", sInteger);
     std::cout << (sInteger == 1 ? "OK" : "FAIL") << "\n";
+
     std::cout << "Double Test => ";
     test.get("DoubleTest", sDecimal);
     std::cout << (sDecimal == 3.141592f ? "OK" : "FAIL") << "\n";
+
     std::cout << "Vector Test => ";
     test.getArray("ArrayTest", sArray);
     std::cout << (sArray[0] == 1 && sArray[1] == 2 && sArray[2] == 3 ? "OK" : "FAIL") << "\n";
+
     std::cout << "Pair Test => ";
     test.getAggregate("PairTest", sPair);
     std::cout << (sPair.first == 42 && sPair.second == 5.12345f ? "OK" : "FAIL") << "\n";
+    
     return (0);
 }
