fa00dc9cef61702b79a4a9289b662bf1290feda5
Maxime Martens <stalker2106x@gmail.com>
2018-02-05 21:00:17 +0100

    updated tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,4 +1,4 @@
-#include "../tinyconf.hpp"
+#include "../include/tinyconf/tinyconf.hpp"
 
 int main(int argc, char **argv)
 {
@@ -40,39 +40,47 @@ int main(int argc, char **argv)
 
     std::cout << "String Test => ";
     test.get("StringTest", sStr);
-    std::cout << (sStr == "OK" ? "OK" : "FAIL") << "\n";
+    std::cout << (sStr == str ? "OK" : "FAIL") << "\n";
 
     std::cout << "Int 8 Test => ";
     test.get("Int8Test", sInteger8);
-    std::cout << (sInteger8 == 1 ? "OK" : "FAIL") << "\n";
+    std::cout << (sInteger8 == integer8 ? "OK" : "FAIL") << "\n";
     
     std::cout << "Int 16 Test => ";
     test.get("Int16Test", sInteger16);
-    std::cout << (sInteger16 == 2 ? "OK" : "FAIL") << "\n";
+    std::cout << (sInteger16 == integer16 ? "OK" : "FAIL") << "\n";
     
     std::cout << "Int 32 Test => ";
     test.get("Int32Test", sInteger32);
-    std::cout << (sInteger32 == 3 ? "OK" : "FAIL") << "\n";
+    std::cout << (sInteger32 == integer32 ? "OK" : "FAIL") << "\n";
     
     std::cout << "Int 64 Test => ";
     test.get("Int64Test", sInteger64);
-    std::cout << (sInteger64 == 4 ? "OK" : "FAIL") << "\n";
+    std::cout << (sInteger64 == integer64 ? "OK" : "FAIL") << "\n";
 
     std::cout << "Float Test => ";
     test.get("FloatTest", sDecimal);
-    std::cout << (sDecimal == 3.141592f ? "OK" : "FAIL") << "\n";
+    std::cout << (sDecimal == decimal ? "OK" : "FAIL") << "\n";
 
     std::cout << "Double Test => ";
     test.get("DoubleTest", sDDecimal);
-    std::cout << (sDDecimal == 3.1415926535f ? "OK" : "FAIL") << "\n";
+    std::cout << (sDDecimal == dDecimal ? "OK" : "FAIL") << "\n";
 
     std::cout << "Vector Test => ";
     test.getArray("ArrayTest", sArray);
-    std::cout << (sArray[0] == 1 && sArray[1] == 2 && sArray[2] == 3 ? "OK" : "FAIL") << "\n";
+    for (size_t i = 0; i < sArray.size(); i++)
+    {
+        if (sArray[i] != array[i])
+        {
+            std::cout << "FAIL\n";
+            break;
+        }
+        if (i == array.size() - 1) std::cout << "OK\n";
+    }
 
     std::cout << "Pair Test => ";
     test.getPair("PairTest", sPair);
-    std::cout << (sPair.first == 42 && sPair.second == 5.12345f ? "OK" : "FAIL") << "\n";
+    std::cout << (sPair.first == pair.first && sPair.second == pair.second ? "OK" : "FAIL") << "\n";
     
     return (0);
 }
